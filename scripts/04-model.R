89687425010a00a28d9082d66aedff2f7e23523c
kaavyakalani26 <kaavya.kalani@mail.utoronto.ca>
2024-03-10 01:14:26 -0500

    Model created

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,37 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Multilinear logistic regression model
+# Author: Kaavya Kalani, Monica Sainani
+# Date: 9 March 2024
+# Contact: kaavya.kalani@mail.utoronto.ca, monica.sainani@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 02-data_cleaning.R
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+survey_data <- arrow::read_parquet("data/analysis_data/survey_data.parquet")
+
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+us_election_model <-
+  stan_glmer(
+    vote_biden ~ sex + (1|age) + (1|state) + (1|education) + (1|income),
+    data = survey_data,
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    cores = 4,
+    adapt_delta = 0.99,
     seed = 853
   )
 
-
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  us_election_model,
+  file = "models/us_election_model_mrp.rds"
 )
-
 
